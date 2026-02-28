4ffe53794830308844c6444b58669de7b02013cd
Touseef Ur Rehman <touseefurrehman5554@gmail.com>
2026-03-01 00:40:00 +0500

    Clean up: Remove unused files

--- clear_db.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from models import db
-
-app = create_app() # Create the app instance
-
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    print("Database tables dropped and recreated successfully.")
